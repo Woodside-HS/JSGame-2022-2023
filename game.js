690a71effb3be3bcba2f26a15bb850e3c2a433d4
Malcolm McDonald <810828@seq.org>
2023-02-13 12:40:56 -0800

    .

--- game.js
+++ game.js
@@ -3,12 +3,14 @@ class Game {
     this.gamePaused = false;
     this.ga = new GameArea();
     this.levels = [];
+    this.hero = new Hero();
   }
   update = function () {
     if (gameState == 0) {
 
     } else if (gameState == 1) {
-
+      this.hero.run()
+      this.levels[1].run();
     }
 
   }
