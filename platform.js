1b19b5bdded3a4066e62e827d15a0f1c12f78887
Evan Demas <112122380+807116@users.noreply.github.com>
2023-02-17 10:54:17 -0800

    enemkes render

--- platform.js
+++ platform.js
@@ -4,39 +4,16 @@ function Platform(x, y, s, ctx, cnvLoc){ // loc from upper left corner and size
     this.size = s;
     this.clr = "brown";
     this.height = 20;
-    this.enemies = [];
     this.cnvLoc = cnvLoc;
+    this.enemies = [];
+    this.loadEnemies(1);
 }
 
-
-//render
-
-
-Platform.prototype.render = function(){
-    for (let i = 0; i < this.enemies.length; i++) {
-        this.enemies[i].run();
-    }
-    let ctx = this.ctx;
-    ctx.beginPath();
-    ctx.strokeStyle = "black";
-    ctx.fillStyle = this.clr;
-    ctx.moveTo(this.loc.x, this.loc.y);
-    ctx.lineTo(this.loc.x+ this.size, this.loc.y);
-    ctx.lineTo(this.loc.x + this.size, this.loc.y + this.height);
-    ctx.lineTo(this.loc.x, this.loc.y + this.height);
-    ctx.lineTo(this.loc.x, this.loc.y);
-    ctx.fill();
-    ctx.stroke();
-    ctx.closePath();
-
- 
-    
-
+Platform.prototype.run = function(){
+    this.render();
+    this.checkChar();
 }
 
-Platform.prototype.loadEnemies = function(n){
-    this.enemies.push(new Enemy(this.loc.x, this.loc.y, this.loc, this.size, this.ctx));
-}
 
 Platform.prototype.checkChar = function(){
     let temp = false;
@@ -50,13 +27,25 @@ Platform.prototype.checkChar = function(){
     hero.onPlatform = temp;
 }
 
-//stops character
-// Platform.prototype.stopChar = function(){
-//     if(h1.loc.x + 10 >= this.loc.x && h1.loc.x -10 <= this.loc.x + this.size){
-//         if(h1.loc.y + 10 >=this.loc.y && h1.loc.y + 10 <this.loc.y+2){
-//           h1.onPlatform = true;
-//           h1.jVel = 0;
-//           check = true;
-//         }
-//     }
-// }
+Platform.prototype.loadEnemies = function(n){
+    this.enemies.push(new Enemy(this.loc.x, this.loc.y, this.loc, this.size, this.ctx));
+}
+
+
+Platform.prototype.render = function(){
+    for (let i = 0; i < this.enemies.length; i++) {
+        this.enemies[i].run();
+    }
+    let ctx = this.ctx;
+    ctx.beginPath();
+    ctx.fillStyle = this.clr;
+    ctx.strokeStyle = "black";
+    ctx.moveTo(this.loc.x, this.loc.y);
+    ctx.lineTo(this.loc.x+ this.size, this.loc.y);
+    ctx.lineTo(this.loc.x + this.size, this.loc.y + this.height);
+    ctx.lineTo(this.loc.x, this.loc.y + this.height);
+    ctx.lineTo(this.loc.x, this.loc.y);
+    ctx.fill();
+    ctx.stroke();
+    ctx.closePath();
+}
