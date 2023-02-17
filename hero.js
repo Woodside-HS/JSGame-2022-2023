6b334984f06263b0403b4f37fc5a7e674ebf5953
Evan Demas <112122380+807116@users.noreply.github.com>
2023-02-17 10:38:00 -0800

    n

--- hero.js
+++ hero.js
@@ -1,5 +1,5 @@
 function Hero(width, ctx) {
-    this.loc = new JSVector(50, 0);
+    this.loc = new JSVector(100, 0);
     this.vel = new JSVector(0, 1);
     this.acc = new JSVector(0,.05);
     this.clr = "blue";
@@ -9,7 +9,6 @@ function Hero(width, ctx) {
     this.jumpCoef = -3;
     this.movingRight = false;
     this.movingLeft = false;
-    this.onPlatform = false;
 
   
 }
@@ -19,39 +18,32 @@ Hero.prototype.run = function () {
     this.render();
     this.update();
     this.checkPlatform();
-    if(!this.onPlatform){
-        this.loc.add(this.vel);
-        this.vel.add(this.acc);
-        this.vel.limit(2);
-    }
-    //this.checkPlatform();
     //this.checkDeath();
+
+}
+
+Hero.prototype.update = function(){
+    this.loc.add(this.vel);
+    this.vel.add(this.acc);
+    
 }
 
 Hero.prototype.jump = function () {
-    //if(count < 4){
     this.loc.y = this.loc.y - 10;
     this.vel.y = this.jumpCoef;
-    this.onPlatform = false;
-    this.vel.limit(2); //limit to twice
-  //  this.count++;
-   // }
+    this.vel.limit(2);
 }
 
-// Hero.prototype.checkPlatform = function(){
-//     for(let i = 0; i<world.platforms.length; i++){
-//         //console.log(world.platforms[i].size);
-//     if(this.loc.x >= world.platforms[i].loc.x && this.loc.x <= world.platforms[i].size+world.platforms[i].loc.x){
-//         console.log(i);
-//         if(this.loc.y >= world.platforms[i].loc.y && this.loc.y <= world.platforms[i].loc.y + world.platforms[i].height){
-//             this.vel.y = 0;
-//             this.loc.y = world.platforms[i].loc.y;
-//             //this.count = 0; //remember to add later
-//         }
-//         //(this.loc.y >= world.platforms[i].loc.y && this.loc.y <= world.platforms[i].loc.y + world.platforms[i].height) && ((this.loc.x >= world.platforms[i].loc.x) && (this.loc.x <= world.platforms[i].size+world.platforms[i].loc.x)) && this.vel.y >=0
-//     }
-// }
-// }
+Hero.prototype.checkPlatform = function(){
+    for(let i=0; i<world.platforms.length; i++){
+        //console.log(world.platforms[i].size);
+        if(this.loc.y >= world.platforms[i].loc.y && this.loc.y <= world.platforms[i].loc.y + world.platforms[i].height && ((this.loc.x >= world.platforms[i].loc.x)) && (this.loc.x <= world.platforms[i].size+world.platforms[i].loc.x)){
+            this.vel.y = 0;
+            this.loc.y = world.platforms[i].loc.y;
+        }
+        //(this.loc.y >= world.platforms[i].loc.y && this.loc.y <= world.platforms[i].loc.y + world.platforms[i].height) && ((this.loc.x >= world.platforms[i].loc.x) && (this.loc.x <= world.platforms[i].size+world.platforms[i].loc.x)) && this.vel.y >=0
+    }
+}
 
 
 Hero.prototype.render = function () {
--- platform.js
+++ platform.js
@@ -4,13 +4,39 @@ function Platform(x, y, s, ctx){ // loc from upper left corner and size
     this.size = s;
     this.clr = "brown";
     this.height = 20;
+    this.enemies = [];
+    this.loadEnemies(1);
 }
 
-Platform.prototype.run = function(){
-    this.render();
-    this.checkChar();
+
+//render
+
+
+Platform.prototype.render = function(){
+    for (let i = 0; i < this.enemies.length; i++) {
+        this.enemies[i].run();
+    }
+    let ctx = this.ctx;
+    ctx.beginPath();
+    ctx.strokeStyle = "black";
+    ctx.fillStyle = this.clr;
+    ctx.moveTo(this.loc.x, this.loc.y);
+    ctx.lineTo(this.loc.x+ this.size, this.loc.y);
+    ctx.lineTo(this.loc.x + this.size, this.loc.y + this.height);
+    ctx.lineTo(this.loc.x, this.loc.y + this.height);
+    ctx.lineTo(this.loc.x, this.loc.y);
+    ctx.fill();
+    ctx.stroke();
+    ctx.closePath();
+
+ 
+    
+
 }
 
+Platform.prototype.loadEnemies = function(n){
+    this.enemies.push(new Enemy(this.loc.x, this.loc.y, this.loc, this.size, this.ctx));
+}
 
 Platform.prototype.checkChar = function(){
     if(this.loc.x <= hero.loc.x && this.loc.x + this.size >= hero.loc.x){
@@ -22,18 +48,13 @@ Platform.prototype.checkChar = function(){
     }
 }
 
-
-Platform.prototype.render = function(){
-    let ctx = this.ctx;
-    ctx.beginPath();
-    ctx.fillStyle = this.clr;
-    ctx.strokeStyle = "black";
-    ctx.moveTo(this.loc.x, this.loc.y);
-    ctx.lineTo(this.loc.x+ this.size, this.loc.y);
-    ctx.lineTo(this.loc.x + this.size, this.loc.y + this.height);
-    ctx.lineTo(this.loc.x, this.loc.y + this.height);
-    ctx.lineTo(this.loc.x, this.loc.y);
-    ctx.fill();
-    ctx.stroke();
-    ctx.closePath();
-}
+//stops character
+// Platform.prototype.stopChar = function(){
+//     if(h1.loc.x + 10 >= this.loc.x && h1.loc.x -10 <= this.loc.x + this.size){
+//         if(h1.loc.y + 10 >=this.loc.y && h1.loc.y + 10 <this.loc.y+2){
+//           h1.onPlatform = true;
+//           h1.jVel = 0;
+//           check = true;
+//         }
+//     }
+// }
