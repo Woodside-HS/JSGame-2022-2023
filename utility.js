756a28357eb6eb391eee18fd2e920f3ebb6ae8dc
SyncedTogether <114436469+AdrianRWilson@users.noreply.github.com>
2023-03-19 11:14:57 -0700

    fix utlitly

--- utility.js
+++ utility.js
@@ -13,7 +13,6 @@ function getRandomColorRGB() {
   const red = Math.floor(Math.random() * 256);
   const green = Math.floor(Math.random() * 256);
   const blue = Math.floor(Math.random() * 256);
-
   return `rgb(${red}, ${green}, ${blue})`;
 }
 
