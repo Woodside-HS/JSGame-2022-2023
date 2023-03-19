8a4884fc3f80044d2c76e9f84461a5d914c50c5e
SyncedTogether <114436469+AdrianRWilson@users.noreply.github.com>
2023-03-19 10:48:53 -0700

    Rewritten main file

--- game.js
+++ game.js
@@ -1,16 +1,17 @@
 class Game {
   constructor() {
-    this.gamePaused = false;
+    // Initiate a new GameArea
     this.ga = new GameArea();
     this.levels = [];
-    this.startX = 200;
-    this.startY = 200;
-    this.hero = new Hero(this.startX, this.startY);
+    this.start = new JSVector(200, 200);
+    this.hero = new Hero(this.start.x, this.start.y);
     this.camLoc = new JSVector(0, 0);
-    this.clickingA = false;
-    this.clickingD = false;
-    this.mouseDown = false;
 
+    [this.gamePaused, this.clickingA, this.clickingD, this.mouseDown].fill(
+      false
+    );
+
+    // Set the dimensions of the game area
     this.dims = {
       top: 0,
       bottom: 600,
@@ -23,47 +24,58 @@ class Game {
     this.levels[0] = new Levels(1);
     this.levels[1] = new Levels(2);
   }
-  update = function () {
+
+  update = () => {
     this.moveCam();
-    if (gameState == 0) {
-      this.hero.statusBlock.isDead = false;
-      this.hero.loc.x = this.startX; // resets the hero location
-      this.hero.loc.y = this.startY; // resets the hero location
-      this.camLoc.x = 0;
-      this.camLoc.y = 0;
-      ctx.font = "50px serif";
-      ctx.fillText("click 'tile 1' to play ", 200, 200);
-      ctx.fillStyle = "green";
-      ctx.strokeStyle = "red";
-      ctx.fill();
-    } else if (gameState == 1) {
-      ctx.save();
-      this.camLoc.x = lerp(this.camLoc.x, this.hero.loc.x - 200, 0.05);
-      ctx.translate(-this.camLoc.x, -this.camLoc.y); //moves the "camera" along the canvas
-      //this.hero.loc.x = this.camLoc.x + 200;
-      //moves the hero with the camera, temporarilly disabled to make sure level works
-      this.hero.run();
-      this.levels[0].run();
-      ctx.restore();
-    } else if (gameState == 2) {
-      ctx.save();
-      ctx.translate(-this.camLoc.x, -this.camLoc.y);
-      this.hero.loc.x = this.camLoc.x + 200;
-      stopMovement = false;
-      this.hero.run();
-      this.levels[1].run();
-      ctx.restore();
-    }
+    // Use a ternary operator to call the appropriate function based on the game state
+    gameState == 0
+      ? this.menuScreen()
+      : gameState == 1
+      ? this.playState()
+      : this.endState();
   };
 
-  moveCam() {
-    //changed to move the hero
-    //if the hero gets too far to one side the camera follows him
-    if (this.clickingA && !hittingRight) {
-      this.hero.loc.x -= 2;
-    }
-    if (this.clickingD && !hittingLeft) {
-      this.hero.loc.x += 2;
-    }
-  }
-} //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++  end Ball constructor
+  // Game state 0
+  menuScreen = () => {
+    // Set the hero's status to not dead, the hero's location to the starting position, and reset the camera position
+    this.hero.statusBlock.isDead = false;
+    this.hero.loc = this.start;
+    this.camLoc.Zero();
+    drawText(
+      ctx,
+      "click 'tile 1' to play ",
+      "50px serif",
+      200,
+      200,
+      "green",
+      "red"
+    );
+  };
+
+  // Game state 1
+  playState = () => {
+    ctx.save();
+    // Camera follow player
+    this.camLoc.x = lerp(this.camLoc.x, this.hero.loc.x - 200, 0.05);
+    ctx.translate(-this.camLoc.x, -this.camLoc.y);
+    this.hero.run();
+    this.levels[0].run();
+    ctx.restore();
+  };
+
+  endState = () => {
+    ctx.save();
+    ctx.translate(-this.camLoc.x, -this.camLoc.y);
+    this.hero.loc.x = this.camLoc.x + 200;
+    stopMovement = false;
+    this.hero.run();
+    this.levels[1].run();
+    ctx.restore();
+  };
+
+  moveCam = () => {
+    // Susbtitude Event handlers
+    this.clickingA && !hittingRight ? (this.hero.loc.x -= 2) : null;
+    this.clickingD && !hittingLeft ? (this.hero.loc.x += 2) : null;
+  };
+}
