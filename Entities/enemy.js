2fb197822de837153dbfa4769fef2d1c87668713
Tucker Gillbrand <810180@seq.org>
2023-03-22 12:19:20 -0700

    Enemy images work

--- Entities/enemy.js
+++ Entities/enemy.js
@@ -7,12 +7,23 @@ class Enemy {
     this.w = w;
     this.move = 1; // the speed of the enemy movement
     this.isdead = false;
+    this.frames = [];
+    this.groove = 0;
+    this.grooveId = 0;
+    this.loadImages();
   }
   run() {
     this.render();
     this.movePlatform();
     this.checkHero();
     this.checkAttack();
+  }
+  loadImages(){
+    for(let i= 0; i<=12;i++){
+      //the 12 has to be hardcoded inn
+      this.frames[i] = document.createElement("img");
+      this.frames[i].src  = "resources/enemy/el"+i+".png";
+    }
   }
   checkAttack() {
     if (game.hero.statusBlock.isAttacking) {
@@ -45,7 +56,7 @@ class Enemy {
   }
   movePlatform() {
     this.loc.x += this.move;
-    if (this.loc.x > this.pLoc.x + this.pWidth - this.w) {
+    if (this.loc.x > this.pLoc.x + this.pWidth - 45) {
       //if the enemy goes to far the movement type gets reversed
       this.move = -1;
     }
@@ -55,16 +66,34 @@ class Enemy {
     }
   }
   render() {
-    ctx.save(); //renders as simple box for now
-    ctx.beginPath();
-    ctx.moveTo(this.loc.x, this.loc.y);
-    ctx.lineTo(this.loc.x + this.w, this.loc.y);
-    ctx.lineTo(this.loc.x + this.w, this.loc.y + this.h);
-    ctx.lineTo(this.loc.x, this.loc.y + this.h);
-    ctx.closePath();
-    ctx.fillStyle = "orange";
-    ctx.fill();
-    ctx.restore();
+    this.groove++;
+    if(this.groove%5 == 0){
+      this.grooveId++;
+      if(this.grooveId == 13){
+        this.grooveId = 0;
+      }
+    }
+    if(this.move < 0){
+      ctx.drawImage(this.frames[this.grooveId], this.loc.x, this.loc.y, 45, 35);
+    } else {
+      ctx.save();
+      ctx.translate(this.loc.x+45, this.loc.y);
+      ctx.scale(-1,1);
+      ctx.drawImage(this.frames[this.grooveId], 0, 0, 45, 35);
+      ctx.setTransform(1, 0, 0, 1, 0, 0);
+      ctx.restore();
+    }
+    
+    // ctx.save(); //renders as simple box for now
+    // ctx.beginPath();
+    // ctx.moveTo(this.loc.x, this.loc.y);
+    // ctx.lineTo(this.loc.x + this.w, this.loc.y);
+    // ctx.lineTo(this.loc.x + this.w, this.loc.y + this.h);
+    // ctx.lineTo(this.loc.x, this.loc.y + this.h);
+    // ctx.closePath();
+    // ctx.fillStyle = "orange";
+    // ctx.fill();
+    // ctx.restore();
   }
 
   checkHero() {
