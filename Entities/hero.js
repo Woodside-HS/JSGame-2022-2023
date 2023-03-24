0097723e3144e46e6956cd302fd54d51a4f08aa8
Tucker Gillbrand <810180@seq.org>
2023-03-23 22:25:55 -0700

    JK
    im gonna cry

--- Entities/hero.js
+++ Entities/hero.js
@@ -40,9 +40,10 @@ class Hero {
 
     //image stuff
     this.moveFrames = [];
-    this.groove = 0;
+    this.groove = 0;//this is the when moving part
     this.grooveId = 0;
     this.loadImages();
+    this.timeSinceMoved = 0;
   }
 
   run() {
@@ -66,23 +67,36 @@ class Hero {
   }
   render() {
     //ctx.drawImage(this.moveFrames[0],this.loc.x,this.loc.y);
-    this.groove++;
+    if(!game.clickingA || !game.clickingD){
+      this.timeSinceMoved++;
+    } else {
+      this.timeSinceMoved = 0;
+      this.groove++;
     if(this.groove%5 == 0){
       this.grooveId++;
       if(this.grooveId>=8){
         this.grooveId = 0;
       }
     }
-    if(this.shootingDirection){
-      ctx.drawImage(this.moveFrames[this.grooveId], this.loc.x+game.camLoc.x,this.loc.y+game.camLoc.y,this.width,this.height);
-    } else{
-      ctx.save();
-      ctx.translate(this.loc.x+this.width, this.loc.y);
-      ctx.scale(-1,1);
-      ctx.drawImage(this.moveFrames[this.grooveId], 0, 0, this.width, this.height);
-      ctx.setTransform(1, 0, 0, 1, 0, 0);
-      ctx.restore();
     }
+    if(this.timeSinceMoved>5){
+      //this only works if it has been more then 5 frames since you have stopped moving
+      //TODO ideally this would be where the idle frames go but we dont have any rn
+      ctx.drawImage(this.moveFrames[0], this.loc.x+game.camLoc.x,this.loc.y+game.camLoc.y,this.width,this.height);
+    } else {
+      if(this.shootingDirection){
+        ctx.drawImage(this.moveFrames[this.grooveId], this.loc.x+game.camLoc.x,this.loc.y+game.camLoc.y,this.width,this.height);
+      } else{
+        ctx.save();
+        ctx.translate(this.loc.x+this.width, this.loc.y);
+        ctx.scale(-1,1);
+        ctx.drawImage(this.moveFrames[this.grooveId], 0, 0, this.width, this.height);
+        ctx.setTransform(1, 0, 0, 1, 0, 0);
+        ctx.restore();
+      }
+      
+    }
+    
     //below is the indicator, gonna keep it for now
     if (game.clickingA) {
       this.indc = -20;
