d92558c32815e678a6160cb5aa2df89a1856a410
AdrianRWilson <114436469+whs811067@users.noreply.github.com>
2023-04-24 12:49:46 -0700

    init

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
---
+++ public/eventlisteners.js
@@ -0,0 +1,107 @@
+const keyDownActions = {
+  // Optimized keydown actions using arrow functions
+  KeyA: () => {
+    if (!stopMovement) game.clickingA = true;
+  },
+  KeyD: () => {
+    if (!stopMovement) game.clickingD = true;
+  },
+  KeyF: () => {
+    game.mouseDown = true;
+  },
+  Space: () => {
+    if (!stopMovement) game.hero.jump();
+  },
+  KeyP: () => {
+    game.hero.shoot();
+  },
+};
+
+const keyUpActions = {
+  // Optimized keyup actions using arrow functions
+  KeyA: () => {
+    game.clickingA = false;
+  },
+  KeyD: () => {
+    game.clickingD = false;
+  },
+  KeyF: () => {
+    game.mouseDown = false;
+  },
+};
+
+window.addEventListener(
+  "keydown",
+  function (event) {
+    const action = keyDownActions[event.code];
+    if (action) {
+      action();
+    }
+  },
+  false
+);
+
+window.addEventListener(
+  "keyup",
+  function (event) {
+    const action = keyUpActions[event.code];
+    if (action) {
+      action();
+    }
+  },
+  false
+);
+
+window.addEventListener(
+  "mousedown",
+  function (event) {
+    game.mouseDown = true;
+    // game.hero.attack();
+    cursor = new JSVector(event.offsetX, event.offsetY);
+    game.hero.cursorLoc.x = cursor.x + game.camLoc.x;
+    game.hero.cursorLoc.y = cursor.y + game.camLoc.y;
+  },
+  false
+);
+
+window.addEventListener(
+  "mouseup",
+  function (event) {
+    game.mouseDown = false;
+  },
+  false
+);
+
+function debugViewCheckBoxClicked() {
+  // Optimized debug view checkbox clicked using ternary functions
+  const { checked } = document.getElementById("DebugViewCheckBox");
+  console.log(`Debug View ${checked ? "Enabled" : "Disabled"}`);
+  debugView = checked;
+  if (checked) {
+    createDebugDOM();
+  }
+}
+
+function createDebugDOM() {
+  // Optimized function to create the DOM for the debug view through looping through an array of properties
+  const debugDiv = document.getElementById("debugDiv");
+  const elementIds = [
+    "PlayerLoc",
+    "PlayerVel",
+    "PlayerCursorLoc",
+    "PlayerHP",
+    "PlayerCoins",
+  ];
+
+  elementIds.forEach((id) => {
+    const element = document.createElement("p");
+    element.id = id;
+    debugDiv.appendChild(element);
+  });
+}
+
+window.addEventListener('click', (event) => {
+  if (event.target === homeModal) {
+    homeModal.style.display = 'none';
+  }
+});
