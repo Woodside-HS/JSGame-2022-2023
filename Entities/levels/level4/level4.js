318575f105ffd983964d66f2bcd7851054d47a0c
806238 <806238@seq.org>
2023-04-24 12:52:18 -0700

    .

--- Entities/levels/level4/level4.js
+++ Entities/levels/level4/level4.js
@@ -1,34 +1,22 @@
 class level4 extends Level{
     constructor(id){
         super(id);   
-        this.walls = [];
         this.loadPlatforms();
         this.loadCoins();
-        this.loadWalls();
-            for(let i = 0;i<this.walls.length;i++){
-                this.walls[i].run();
-            } 
     }
 
     loadPlatforms() {
-        this.platforms[0] = new level4platform(150,670,300);
-        this.platforms[1] = new level4platform(550,670,100);
-        this.platforms[2] = new level4platform(750,620,100);
-        this.platforms[3] = new level4platform(950,620,100);
-        this.platforms[4] = new level4platform(1050,500,100);
-        this.platforms[5] = new level4platform(1250,500,100);
-        this.platforms[6] = new level4platform(1450,670,100);
-        this.platforms[7] = new level4platform(1650,670,100);
-        this.platforms[8] = new level4platform(1850,670,100);
+        this.platforms[0] = new level4platform(150,670,300,true);
+        this.platforms[1] = new level4platform(550,670,100,true);
+        this.platforms[2] = new level4platform(750,620,100,true);
+        this.platforms[3] = new level4platform(950,620,100,true);
+        this.platforms[4] = new level4platform(1050,500,100,true);
+        this.platforms[5] = new level4platform(1250,500,100,true);
+        this.platforms[6] = new level4platform(1450,670,100,true);
+        this.platforms[7] = new level4platform(1650,670,100,true);
+        this.platforms[8] = new level4platform(1850,670,100,true);
+        this.platforms[8] = new level4platform(1850,670,100,false);
 
         
     }
-
-    loadWalls(){
-        this.walls[0] = new Wall(100,200,100);
-    }
-
-    
-
-
 }
--- Entities/levels/level4/level4platform.js
+++ Entities/levels/level4/level4platform.js
@@ -1,16 +1,27 @@
 class level4platform extends Platform {
-    constructor(x,y,w){
+    constructor(x,y,w,isFloor){
         super(x,y,w);
+        this.isFloor = isFloor;
+        if(this.isFloor === false){
+            this.loc.x = this.loc.x-this.width
+        }
     }
     run(){
-        this.update;
-        this.render();
+        this.update();
+        if(this.isFloor){
         this.checkHero();
+        this.renderFloor();
+        } else {
+            this.renderWall();
+            this.checkHeroWall();
+        }
+        
     }
     update() {
+
     }
 
-    render() {
+    renderFloor() {
         ctx.beginPath();
         ctx.moveTo(this.loc.x,this.loc.y);
         ctx.lineTo(this.loc.x+this.width,this.loc.y);
@@ -19,5 +30,30 @@ class level4platform extends Platform {
         ctx.closePath();
         ctx.fillStyle = "purple";
         ctx.fill();
+    }
+
+    renderWall(){
+        ctx.beginPath();
+        ctx.moveTo(this.loc.x,this.loc.y);
+        ctx.lineTo(this.loc.x+10,this.loc.y);
+        ctx.lineTo(this.loc.x+10,this.loc.y-this.width);
+        ctx.lineTo(this.loc.x,this.loc.y-this.width);
+        ctx.closePath();
+        ctx.fillStyle = "red";
+        ctx.fill();
+    }
+
+    checkHeroWall() {
+            let heroLoc = new JSVector(game.hero.loc.x, game.hero.loc.y); // the heros x & y location
+            let heroH = game.hero.height; // the heros height
+            let heroW = game.hero.width; // the heros width
+            if (
+                heroLoc.x + heroW > this.loc.x &&
+                heroLoc.x < this.loc.x + this.width &&
+                heroLoc.y + heroH > this.loc.y &&
+                heroLoc.y < this.loc.y + this.height
+            ){
+                console.log("collision")
+            }
     }
 }
