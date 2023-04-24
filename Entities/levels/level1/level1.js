daa387b2dd2c2b4ea0d01eea1f0a027ca55be5aa
Tucker Gillbrand <810180@seq.org>
2023-04-24 10:38:55 -0700

    Added more platforms

--- Entities/levels/level1/level1.js
+++ Entities/levels/level1/level1.js
@@ -5,7 +5,8 @@ class level1 extends Level{
         this.loadCoins();
     }
     loadPlatforms() {
-        this.platforms[0] = new level1platform(150,400,100);
+        this.platforms[0] = new level1platform(250,550,100);
+        this.platforms[1] = new level1platform(0,650,6000);//! big ground platform
         //need the specific platform class to get started on loading platforms
         
     }
