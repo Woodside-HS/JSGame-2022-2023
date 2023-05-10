47426f7e37881303ac027913d3b54fb10215a132
Malcolm McDonald <810828@seq.org>
2023-05-10 12:49:07 -0700

    starting the attack functions for the zombie

--- Entities/levels/level5/zombie.js
+++ Entities/levels/level5/zombie.js
@@ -10,24 +10,70 @@ class zombie {
         this.movingL = true;
         this.movingR = false
         this.movingSpeed = 0.5
+        this.attacking = false;
+        this.lookingL = false;
+        this.lookingR = false
+        this.attackTimer = 0;
     }
     run() {
         this.render();
         this.update();
         this.checkHero()
         this.moveZombie()
+        this.attackHero();
     }
     render() {
         ctx.save()
         ctx.translate(this.loc.x, this.loc.y);
         ctx.rect(0, 0, this.h, this.w)
+        if (this.lookingR) {
+            ctx.moveTo(0, -10)
+            ctx.lineTo(100, -10);
+        } else if (this.lookingL) {
+            ctx.moveTo(0, -10)
+            ctx.lineTo(-100, -10);
+        }
+        ctx.stroke()
         ctx.fill()
         ctx.restore()
         // console.log("shoulde be here")
     }
+    attackHero() {
+        if (this.isAtacking) {
+            if (this.attackTimer++ >= 100) {
+                this.isAtacking = false;
+                this.movingL = true;
+                this.attackTimer = 0;
+            }
+            if (game.hero.loc.x > this.loc.x) {
+                this.lookingR = true;
+                this.lookingL = false;
+            } else {
+                this.lookingL = true;
+                this.lookingR = false;
+            }
+        }
+
+    }
     update() {
     }
     checkHero() {
+        let heroLoc = new JSVector(game.hero.loc.x, game.hero.loc.y); // the heros x & y location
+        let heroH = game.hero.height; // the heros height
+        let heroW = game.hero.width; // the heros width
+        if (
+            //checks if the heros location is overlaping with the platform
+            heroLoc.x + heroW > this.loc.x &&
+            heroLoc.x < this.loc.x + this.w &&
+            heroLoc.y + heroH > this.loc.y &&
+            heroLoc.y < this.loc.y + this.h
+        ) {
+            this.movingL = false;
+            this.movingR = false;
+            this.isAtacking = true;
+            return true;
+        }
+        return false;
     }
     moveZombie() {
         if (this.movingL && !this.movingR) { // moving the zombie Left
@@ -39,10 +85,14 @@ class zombie {
         if (this.loc.x + this.w >= this.platformLoc.x + this.platformWidth) {
             this.movingR = false;
             this.movingL = true;
+            this.lookingL = true;
+            this.lookingR = false;
         }
         if (this.loc.x <= this.platformLoc.x) {
             this.movingL = false;
             this.movingR = true;
+            this.lookingR = true;
+            this.lookingL = false;
         }
     }
 }
