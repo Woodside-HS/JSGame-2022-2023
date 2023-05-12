5ee9b7cf7227055c48f3419d17a4da354c42215c
Tucker Gillbrand <810180@seq.org>
2023-05-12 11:57:09 -0700

    bvcxbhttps://officialbookserver5.blogspot.com/id/B01APTZM12cb

--- Entities/levels/level1/level1.js
+++ Entities/levels/level1/level1.js
@@ -11,7 +11,7 @@ class level1 extends Level{
         this.platforms[0] = new level1platform(150,500,150, false);
         this.platforms[1] = new level1platform(0,650,150, false);//! big ground platform
         this.platforms[2] = new level1platform(400,550,150, true);
-        //need the specific platform class to get started on loading platforms
+        this.platforms[3] = new level1platform(600,675,150, false);
     }
     loadFreeEnemies(){
         this.enemies[0] = new lvl1Enemy1(25,650,0,150,200);
