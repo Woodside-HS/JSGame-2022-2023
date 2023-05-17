6d9ff423e30e66290522e9c0ac372ccd8be3b7b8
Malcolm McDonald <810828@seq.org>
2023-05-17 09:12:30 -0700

    moving everything into the public file

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const port = 3000
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+app.use((req, res) => {
+    res.status(404);
+    res.send("404 lmao")
 });
 
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
