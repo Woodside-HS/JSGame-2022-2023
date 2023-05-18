5e194101fc8d621664c5aceac3b7c528181c2911
Tucker Gillbrand <810180@seq.org>
2023-05-18 11:43:46 -0700

    Added like a bajillion levels

--- Entities/levels/level1/level1.js
+++ Entities/levels/level1/level1.js
@@ -17,6 +17,30 @@ class level1 extends Level{
         this.platforms[6] = new level1platform(950,500,150,true);
         this.platforms[7] = new level1platform(900,650,150,false);
         this.platforms[8] = new level1platform(1150,450,150,false);
+        this.platforms[9] = new level1platform(1075,700,150,false);
+        this.platforms[10] = new level1platform(1400,600,150,false);
+
+        this.platforms[11] = new level1platform(1500,650,150, false);//! big ground platform
+        this.platforms[12] = new level1platform(1750,550,150, true);
+        this.platforms[13] = new level1platform(1900,675,150, false);
+        this.platforms[14] = new level1platform(2150,550,150,false);
+        this.platforms[15] = new level1platform(2750,600,150,false)
+        this.platforms[16] = new level1platform(2450,500,150,true);
+        this.platforms[17] = new level1platform(2400,650,150,false);
+        this.platforms[18] = new level1platform(2650,450,150,false);
+        this.platforms[19] = new level1platform(2275,700,150,false);
+        this.platforms[20] = new level1platform(2900,600,150,false);
+
+        this.platforms[21] = new level1platform(3000,650,150, false);//! big ground platform
+        this.platforms[22] = new level1platform(3250,550,150, true);
+        this.platforms[23] = new level1platform(3400,675,150, false);
+        this.platforms[24] = new level1platform(3550,650,150,false);
+        this.platforms[25] = new level1platform(3750,600,150,false)
+        this.platforms[26] = new level1platform(4050,500,150,true);
+        this.platforms[27] = new level1platform(4000,650,150,false);
+        this.platforms[28] = new level1platform(3950,450,150,false);
+        this.platforms[29] = new level1platform(3875,700,150,false);
+        this.platforms[30] = new level1platform(4200,600,150,false);
     }
     loadFreeEnemies(){
         this.enemies[0] = new lvl1Enemy1(25,650,0,150,200);
