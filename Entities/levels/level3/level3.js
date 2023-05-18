493ecf51524c3ea8b03a7edb6e41f1fc4ae26d5e
Spencer Bergland <809083@seq.org>
2023-05-18 00:19:48 -0700

    Where are the backgrounds rendered?

--- Entities/levels/level3/level3.js
+++ Entities/levels/level3/level3.js
@@ -61,12 +61,14 @@ class level3 extends Level{
         }
         */
     }
-    // loadBackgrounds(){
-    //     for(let i = 1; i <=3; i++){
-    //         this.backgroundImages[i] = document.createElement("img");
-    //         this.backgroundImages[i].src = "Images/Level3/Lvl3Background/bg" + this.id + ".png";
-    //     }
-    // }
+    loadBackgrounds(){ //!WHy is this not working?
+        // let backgrounds = []; 
+        // for(let i = 1; i <=3; i++){
+        //     backgrounds[i-1] = document.createElement("img");
+        //     backgrounds[i-1].src = "Images/Level3/Lvl3Background/bg" + this.id + ".png";
+        // }
+        // this.background = new genericBackground(backgrounds);
+    }
     loadEnemy1Images(){
         // template function
         // for(let i = 0; i < 8; i++){
