80a9bbaf8ed504ab80c728050ce92adf604f103e
AdrianRWilson <112122380+807116@users.noreply.github.com>
2023-05-18 09:41:39 -0700

    working platform

--- Entities/levels/level2/level2.js
+++ Entities/levels/level2/level2.js
@@ -1,110 +1,13 @@
-/*
-Stuff to add
-* All of the resources & platforms
-* have a program to load images
-* load background images(and parallax)
-* Run function(runs all entities)
-* Render function()
-*/
-class level2 {
+class level2 extends Level {
     constructor(id) {
-        this.platforms = [];
-        this.background;
-        this.resources = [];
-        this.coins = [];
-        this.enemies = [];
-        this.platformImages = [];
-        this.id = id;
-        this.enemy1Images = [];
-        this.enemy2Images = [];
+        super(id);
+        this.loadPlatforms();
+        this.loadCoins();
+        this.loadBackgrounds(3);
     }
-    
-    run(){
-        this.update();
-        this.render();
-    }
-
     loadPlatforms() {
-        this.platforms[0] = new Platform(200, 400, 200);
+        this.platforms[0] = new level1platform(150, 690, 10000);
+        //need the specific platform class to get started on loading platforms
 
-    }
-
-    loadResources(pArray1, pArray2) {//same powerups
-        //pArray1 is the array of platforms with powerup 1 
-        //pArray2 is the array of platforms with powerup 2
-        //which powerup goes into which platform is hardcoded
-        
-    }
-    
-    loadEnemies(pArray1, pArray2) { //Trash enemies
-        //pArray1 is the array of platforms with enemy 1 
-        //pArray2 is the array of platforms with enemy 2
-        //hardcoding which type of enemies
-    }
-
-    loadPlatformImages(){//Scaffolding platforms
-        // template function 
-        /*
-        for(let i = 0; i < platforms.length; i++){
-            this.platformImages[i] = document.createElement("img");
-            this.platformImages[i].src = "resources/Platform/platform1" + this.id + ".png";
-        }
-        // */
-    }
-    loadBackgrounds(num){
-        let backgrounds = [];
-        for(let i = 0; i < num; i++){
-            backgrounds[i] = document.createElement("img");
-            let path = "Images/Level" + this.id +"/Lvl" + this.id + "Background/bg" + (i+1)+".png";
-            //the images need to be set in order from furthest back to furthest forward
-            console.log(path);
-            backgrounds[i].src = path;
-        }
-        this.background = new genericBackground(backgrounds);
-        
-    }
-    loadEnemy1Images(){
-        // template function
-        // for(let i = 0; i < 8; i++){
-        //     this.enemy1Images[i] = document.createElement("img");
-        //     this.enemy1Images[i].src  = "resources/enemy/el"+i+".png";
-        // }
-    }
-    loadEnemy2Images(){
-        // template function
-        // for(let i = 0; i < 8; i++){
-        //     this.enemy2Images[i] = document.createElement("img");
-        //     this.enemy2Images[i].src  = "resources/enemy/el"+i+".png";
-        // }
-    }
-    
-
-    loadCoins() {
-        this.platforms.forEach(platform => {
-            let halfNhalf = Math.random() * 100;
-            if (halfNhalf < 50) {
-                platform.coin = new Coin(platform.x, platform.y, 20, 20);
-                console.log('platform.coin: ', platform.coin);
-            }
-        });
-    }
-
-    update() {
-        // template function
-    }
-    
-    render() {
-        this.platforms.forEach(platform => { //goes through array of platforms and runs them
-            platforms.run();
-        });
-        this.resources.forEach(resource => {
-            resource.run();
-        });
-        this.enemies.forEach(enemy => {
-            enemy.run();
-        });
-        this.coins.forEach(coin => {
-            coin.run();
-        });
     }
 }
--- Entities/levels/level2/level2Platform.js
+++ Entities/levels/level2/level2Platform.js
@@ -1,69 +1,23 @@
 class level2platform extends Platform {
-    constructor(x, y, w, ynE) {
+    constructor(x, y, w) {
         super(x, y, w);
-        // this.enemy = []
-        this.ynZ = ynZ; // yes/no trash enemy
-        if (this.ynE == true) {
-            this.enemy = new zombie(x, y, 20, 20, this.width)
-            // this.loadTrashEnemy()
-            // console.log(this.ynZ)
-        }
     }
     run() {
-        this.update()
+        this.update;
         this.render();
         this.checkHero();
     }
     update() {
-        if (this.enemy) {
-            this.enemy.run()
-        }
     }
-    updateZombie() {
-        for (let i = 0; i < this.enemy.length; i++) {
-            if (this.enemy[i].isDead) {
-                this.enemy.splice(i, 1)
-            }
-        }
-    }
-    checkHero() {
-        let heroLoc = new JSVector(game.hero.loc.x, game.hero.loc.y); // the heros x & y location
-        let heroH = game.hero.height; // the heros height
-        let heroW = game.hero.width; // the heros width
-        if (
-            //checks if the heros location is overlaping with the platform
-            heroLoc.x + heroW > this.loc.x &&
-            heroLoc.x < this.loc.x + this.width &&
-            heroLoc.y + heroH > this.loc.y &&
-            heroLoc.y < this.loc.y + this.height
-        ) {
-            // console.log("touching platform");
-            if (game.hero.vel.y > 0) {
-                // checks if the hero is falling
-                game.hero.statusBlock.jumpCount = 0;
-                game.hero.vel.y = 0;
-                game.hero.loc.y = this.loc.y - game.hero.height; // places the hero on the top of the platform
-            } else {
-                // makes it so you cant go through the bottom of the platform
-                game.hero.vel.y = 0;
-                game.hero.loc.y = this.loc.y + this.height; // sets the hero the bottom of the platform so it wont go past
-            }
-            game.hero.statusBlock.onPlatform = true;
-            return true;
-        }
-        return false;
-    }
+
     render() {
-        ctx.save()
         ctx.beginPath();
         ctx.moveTo(this.loc.x, this.loc.y);
         ctx.lineTo(this.loc.x + this.width, this.loc.y);
         ctx.lineTo(this.loc.x + this.width, this.loc.y + 10);
         ctx.lineTo(this.loc.x, this.loc.y + 10);
         ctx.closePath();
-        ctx.fillStyle = "yellow";
+        ctx.fillStyle = "red";
         ctx.fill();
-        ctx.restore()
     }
-
 }
