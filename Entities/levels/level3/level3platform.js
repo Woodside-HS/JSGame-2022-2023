12c7d3e6f176656b5b9ab5f56e5ded5efbbf2708
Spencer Bergland <809083@seq.org>
2023-05-24 11:26:50 -0700

    Changes now need to test

--- Entities/levels/level3/level3platform.js
+++ Entities/levels/level3/level3platform.js
@@ -7,22 +7,36 @@ class Level3Platform extends Platform {
         super(x, y, w);
         this.isDead = false;
         this.poisonOak = p;
+        this.pCounter = 100;
     }
 
     run() {
         if(!this.isDead){
             this.update();
-            this.render();
+            this.renderN(); //normal render
             this.checkHero();
             this.sideCollisions();
         }
+        if(this.poisonOak){
+            if(this.pCounter%5){
+                this.checkHero.health--;
+            }
+            this.hero.loc.y += Math.random()*6-3;
+            this.pCounter--;
+            this.renderP();
+            if(this.pCounter<2){
+                this.poisonOak = false;
+            }
+
+        }
+
     }
 
     update() {
         // template function
     }
 
-    render() {
+    renderN() {
         ctx.beginPath();
         ctx.moveTo(this.loc.x,this.loc.y);
         ctx.lineTo(this.loc.x+this.width,this.loc.y);
@@ -31,6 +45,18 @@ class Level3Platform extends Platform {
         ctx.closePath();
         ctx.fillStyle = "green";
         ctx.fill();
+    }
+
+    renderP(){
+        ctx.beginPath();
+        ctx.moveTo(this.loc.x,this.loc.y);
+        ctx.lineTo(this.loc.x+this.width,this.loc.y);
+        ctx.lineTo(this.loc.x+this.width,this.loc.y+10);
+        ctx.lineTo(this.loc.x,this.loc.y+10);
+        ctx.closePath();
+        ctx.fillStyle = "red";
+        ctx.fill();
+
     }
 
     checkHero() {
