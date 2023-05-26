3820782e0d6c7e0a7aac1c0c177f7c02f5cb72cc
Malcolm McDonald <810828@seq.org>
2023-05-25 18:36:09 -0700

    adding platform images

--- Entities/levels/level5/level5Platform.js
+++ Entities/levels/level5/level5Platform.js
@@ -8,12 +8,18 @@ class level5platform extends Platform {
             // this.loadZombie()
             // console.log(this.ynZ)
         }
+        this.shortPlatformImg;
     }
     run() {
+        this.loadImages();
         this.update()
         this.render();
         this.checkHero();
         this.updateZombie();
+    }
+    loadImages() {
+        this.shortPlatformImg = document.createElement("img");
+        this.shortPlatformImg.src = `Images/Level5/platforms/plat1.png`
     }
     update() {
         if (this.enemy) {
@@ -27,14 +33,16 @@ class level5platform extends Platform {
     }
     render() {
         ctx.save()
-        ctx.beginPath();
-        ctx.moveTo(this.loc.x, this.loc.y);
-        ctx.lineTo(this.loc.x + this.width, this.loc.y);
-        ctx.lineTo(this.loc.x + this.width, this.loc.y + 10);
-        ctx.lineTo(this.loc.x, this.loc.y + 10);
-        ctx.closePath();
-        ctx.fillStyle = this.clr;
-        ctx.fill();
+        // ctx.beginPath();
+        // ctx.moveTo(this.loc.x, this.loc.y);
+        // ctx.lineTo(this.loc.x + this.width, this.loc.y);
+        // ctx.lineTo(this.loc.x + this.width, this.loc.y + 10);
+        // ctx.lineTo(this.loc.x, this.loc.y + 10);
+        // ctx.closePath();
+        // ctx.fillStyle = this.clr;
+        // ctx.fill();
+
+        ctx.drawImage(this.shortPlatformImg, this.loc.x, this.loc.y, this.width, 10);
         ctx.restore()
     }
 
