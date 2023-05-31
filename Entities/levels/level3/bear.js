44e553cc0ec12c5243bcf12c9b4d26cbaaef3bee
Spencer Bergland <809083@seq.org>
2023-05-31 11:14:29 -0700

    Bear needs to attack and idle

--- Entities/levels/level3/bear.js
+++ Entities/levels/level3/bear.js
@@ -1,11 +1,17 @@
 class Bear{
-    constructor(){
+    constructor(platform){
            this.docile = false
            this.speed = 2;
 
     }
 
     run(){
+        if(this.docile){
+            this.attack();
+        }
+        else{
+            this.idle();
+        }
 
     } 
 
@@ -21,7 +27,14 @@ class Bear{
     
     befriended(){
         this.docile = true;
-        
+    }
+
+    render(){
+        ctx.beginPath();
+        ctx.arc(this.loc.x, this.loc.y, 10, 0, Math.PI*2);
+        ctx.closePath();
+        ctx.fillStyle = "black";
+        ctx.fill();
     }
 
 }
