bb6ae7e3cd4e17e068ad2c095475696a83ba0f2f
Spencer Bergland <809083@seq.org>
2023-06-05 22:18:34 -0700

    .

--- Entities/levels/level3/level3.js
+++ Entities/levels/level3/level3.js
@@ -2,7 +2,7 @@ class level3 extends Level{
     constructor(id, levels){
         super(id);   
         this.loadPlatforms();
-        this.end = Date.now() + 120000;
+        this.end = Date.now() + 60000;
         this.loadBackgrounds(3);
         this.loadCoins();
         this.loadResources([7],[5],[6]);
